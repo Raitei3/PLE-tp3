4e83f12381a213f24f3a968c65dd199931419906
Antoine Borde <antoine24.borde@gmail.com>
2017-10-15 23:13:36 +0200

    implement Data

--- src/main/java/BigData/TP3.java
+++ src/main/java/BigData/TP3.java
@@ -1,12 +1,15 @@
 package BigData;
+import java.io.DataInput;
+import java.io.DataOutput;
 import java.io.IOException;
-
+import java.util.StringJoiner;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.Writable;
 import org.apache.hadoop.mapreduce.Counter;
 import org.apache.hadoop.mapreduce.Counters;
 import org.apache.hadoop.mapreduce.Job;
@@ -14,6 +17,7 @@ import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.MultipleOutputs;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
@@ -28,7 +32,7 @@ public enum WCP{
 	};
 
   public static class TP3Mapper
-       extends Mapper<Object, Text, NullWritable, Text>{
+       extends Mapper<Object, Text, Text, Data>{
 
 	  		public void map(Object key, Text value, Context context
 			  ) throws IOException, InterruptedException {
@@ -40,37 +44,93 @@ public enum WCP{
 	  				String[] parts = value.toString().split(",");
 	  				if (parts.length == 7){
 							try{
-	  					     context.getCounter(WCP.total_pop).increment(Integer.parseInt(parts[4]));
-	  				       context.write(NullWritable.get(), value);
+								int nbPop=Integer.parseInt(parts[4]);
+	  					     context.getCounter(WCP.total_pop).increment(nbPop);
+	  				     context.write(new Text(parts[0]), new Data(value, 1, nbPop));
 					            }catch (NumberFormatException e) {}
 					                 }
 	  			  }
 	  		}
   }
   public static class TP3Reducer
-       extends Reducer<NullWritable,Text,NullWritable,Text> {
-    public void reduce(NullWritable key, Text values,
+       extends Reducer<Text,Data,NullWritable,Text> {
+	  private MultipleOutputs<NullWritable, Text> multOut;
+		public void setup(Context context) {
+			this.multOut = new MultipleOutputs<NullWritable, Text>(context);
+		}
+		
+		
+    public void reduce(Text key, Iterable<Data> values,
                        Context context
                        ) throws IOException, InterruptedException {
-      context.write(key, values);
+    	
+		StringJoiner sj = new StringJoiner("\n");
+		int cities = 0;
+		int pop = 0;
+		for (Data value : values) {
+			sj.add(value.entries.toString());
+			cities++;
+			pop += value.pop.get();
+		}
+		
+		Text entries = new Text(sj.toString());
+		Text entry = new Text(String.format("%s,%d,%d", key.toString(), cities, pop));
+		
+		this.multOut.write("entries", NullWritable.get(), entries);
+		this.multOut.write("countries", NullWritable.get(), entry);
     }
   }
-  public static void main(String[] args) throws Exception {
+  
+  
+
+  
+  public static class  Data implements Writable {
+		public Text entries;
+		public IntWritable cities;
+		public IntWritable pop;
+		
+		public Data() {
+			this.entries = new Text();
+			this.cities = new IntWritable();
+			this.pop = new IntWritable();
+		}
+		
+		public Data(Text entries, int cities, int pop) {
+			this.entries = entries;
+			this.cities = new IntWritable(cities);
+			this.pop = new IntWritable(pop);
+		}
+
+		public void readFields(DataInput arg0) throws IOException {
+			// TODO Auto-generated method stub
+			
+		}
+
+		public void write(DataOutput arg0) throws IOException {
+			// TODO Auto-generated method stub
+			
+		}
+  }
+  
+  
+    public static void main(String[] args) throws Exception {
     Configuration conf = new Configuration();
     System.out.println("coucou");
     Job job = Job.getInstance(conf, "TP3");
     job.setNumReduceTasks(1);
     job.setJarByClass(TP3.class);
     job.setMapperClass(TP3Mapper.class);
-    job.setMapOutputKeyClass(NullWritable.class);
-    job.setMapOutputValueClass(Text.class);
+    job.setMapOutputKeyClass(Text.class);
+    job.setMapOutputValueClass(Data.class);
     job.setReducerClass(TP3Reducer.class);
-    job.setOutputKeyClass(Text.class);
-    job.setOutputValueClass(IntWritable.class);
+    job.setOutputKeyClass(NullWritable.class);
+    job.setOutputValueClass(Text.class);
     job.setOutputFormatClass(TextOutputFormat.class);
     job.setInputFormatClass(TextInputFormat.class);
     FileInputFormat.addInputPath(job, new Path(args[0]));
     FileOutputFormat.setOutputPath(job, new Path(args[1]));
+    MultipleOutputs.addNamedOutput(job, "entries", TextOutputFormat.class, NullWritable.class, Text.class);
+    MultipleOutputs.addNamedOutput(job, "countries", TextOutputFormat.class, NullWritable.class, Text.class);
     System.exit(job.waitForCompletion(true) ? 0 : 1);
     Counters count = job.getCounters();
     Counter nb_pop = count.findCounter(WCP.nb_pop);
@@ -80,4 +140,8 @@ public enum WCP{
     System.out.println(nb_pop.getDisplayName() + ":" + nb_pop.getValue());
     System.out.println(nb_cities.getDisplayName() + ":" + nb_cities.getValue());
   }
+  
+  
+
 }
+
